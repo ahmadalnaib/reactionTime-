bcb7649744ffe873e19b773c8caa64fad79f2a8e
ahmedhabeeb73 <alnaib888@gmail.com>
2025-05-06 19:11:02 +0200

    start

---
+++ vue-project/src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import Block from './components/Block.vue';
+export default {
+  components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: 0,
+      showResult: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000+ Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResult = false;
+    },
+      endGamge(reactionTime) {
+        this.isPlaying = false;
+        this.score = reactionTime;
+        this.showResult = true;
+        
+      },
+    },
+  
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>Welcome to Your Vue.js App</h1>
+    <p>This is a simple Vue.js application.</p>
+    <p v-if="showResult">reactionTime is {{ score }}ms</p>
+  </div>
+  <Block v-if="isPlaying" :delay="delay" @reactionTime="endGamge"/>
+  <button @click="start" :disabled="isPlaying">Play</button>
+</template>
---
+++ vue-project/src/components/Block.vue
@@ -0,0 +1,64 @@
+<script>
+
+export default{
+  props:{
+    delay:{
+      type:Number,
+
+    }
+  },
+  data(){
+    return{
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+     
+    }
+  },
+
+  mounted(){
+  setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+
+  updated(){
+
+  },
+
+  methods:{
+    startTimer(){
+  this.timer=setInterval(() => {
+    this.reactionTime +=10;
+  }, 10);
+    },
+    stopTimer(){
+      clearInterval(this.timer);
+      this.timer = null;
+      this.$emit('reactionTime', this.reactionTime);
+    }
+
+  },
+}
+
+</script>
+
+<template>
+<div v-if="showBlock" @click="stopTimer" >
+  Click me
+</div>
+</template>
+
+<style scoped>
+div {
+  width: 400px;
+  height: 400px;
+  background-color: #42b983;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 20px;
+}
+</style>
