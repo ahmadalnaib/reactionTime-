d27e2756b3005c57828248fcedce94d307ad3624
ahmedhabeeb73 <alnaib888@gmail.com>
2025-05-07 17:59:16 +0200

    add project

--- project-new/src/views/HomeView.vue
+++ project-new/src/views/HomeView.vue
@@ -1,9 +1,45 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script >
+
+export default{
+  data(){
+    return{
+      
+      projects: []
+
+    }
+  },
+  mounted(){
+    // fetch data from api
+    // fetch('http://localhost:3000/projects')
+    // return promise
+    fetch('http://localhost:3000/projects')
+    // return promise
+    .then(response => response.json())
+    // convert to json
+    // then return data
+    // then set data to projects
+    // set data to projects
+    .then(data => {
+      this.projects = data
+    })
+    .catch(error => {
+      console.error('Error fetching projects:', error);
+    });
+  }
+}
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main v-if="projects.length">
+    <h1>Projects</h1>
+    <p>Here are some of the projects I have worked on:</p>
+<div v-for="project in projects" :key="project.id">
+  <h1>{{ project.name }}</h1>
+  <p>{{ project.description }}</p>
+
+  <hr />
+
+</div>
   </main>
 </template>
